85963a8c94126fb9f2bcebeb12fd0f0d8eb4db8d
Ralf-Renz-Estellana-Bantilo <ralfrenzbantilo853@gmail.com>
2021-10-13 13:02:51 +0800

    deleted some junk codes

--- src/Home-Folder/PostContent.js
+++ src/Home-Folder/PostContent.js
@@ -35,49 +35,6 @@ export class PostContent extends Component {
 				}}
 				className='post-content'
 				style={showMore ? { height: "0" } : { height: "auto" }}>
-				{/* <h3 className='job-title'>{jobTitle}</h3>
-
-				<div className='post-detail-container'>
-					<div className='post-detail-group1'>
-						<div className='post-detail'>
-							<p>Job Category:</p>
-							<h4>{this.props.info.category}</h4>
-						</div>
-						<div className='post-detail'>
-							<p>Required No. of Employees:</p>
-							<h4>{this.props.info.reqNoEmp}</h4>
-						</div>
-						<div className='post-detail'>
-							<p>Salary:</p>
-							<h4>{this.props.info.salary}</h4>
-						</div>
-					</div>
-
-					<div className='post-detail-group2'>
-						<div className='post-detail'>
-							<p>Job Type:</p>
-							<h4>{jobType}</h4>
-						</div>
-						<div className='post-detail'>
-							<p>Preferred Sex:</p>
-							<h4>{prefSex}</h4>
-						</div>
-						<div className='post-detail'>
-							<p>Job Vacancy Status:</p>
-							<div className='active-status'>
-								<div
-									className='active-circle'
-									style={
-										this.props.info.status === "Active"
-											? { backgroundColor: "#00ff40" }
-											: { backgroundColor: "#ff0000" }
-									}></div>
-								<h4>{status}</h4>
-							</div>
-						</div>
-					</div>
-				</div> */}
-
 				<div ref='out' className='job-qualification-container'>
 					<div className='job-qualification-portion'>
 						<h3>--- Job Qualifications ---</h3>
